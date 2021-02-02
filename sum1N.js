78e12d106501a1d1c0f83b7b70ed0f7621bbd8c1
Sandeep Pandita <sandeeppandita46@gmail.com>
2021-02-02 16:03:33 -0500

    Sum functions performance measured

--- sum1N.js
+++ sum1N.js
@@ -1,8 +1,28 @@
-function sumNumbers(num) {
+const { performance } = require('perf_hooks');
+// Set up
+function sumNumbersLoop(num) {
   let total = 0;
   for (let i = 1; i <= num; i++) {
     total += i;
   }
   console.log(total);
 }
-sumNumbers(10);
+
+function sumNumbersFormula(num) {
+  let total = (num * (num + 1)) / 2;
+  console.log(total);
+}
+
+// Execution
+let num = 100;
+let t1 = performance.now();
+sumNumbersLoop(num); // O(n)
+let t2 = performance.now();
+
+console.log('Loop Execution Took ', t2 - t1, 'micro seconds.');
+
+let t3 = performance.now();
+sumNumbersFormula(num); // O(1)
+let t4 = performance.now();
+
+console.log('Formula Execution Took ', t4 - t3, 'micro seconds.');
